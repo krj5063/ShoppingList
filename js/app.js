13f9b03d89d99150f2f867aa254f599614bea083
krj5063 <krj5063@gmail.com>
2014-11-29 13:35:39 -0500

    Finished JQuery function
    desfsd

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 $(document).ready(function() { 
 
-    $(".pure-input-1-2")
-    .keyup(function() {
-    var value = $( this ).val();
-    $(".popup").text( value );
-    })
-    .keyup(); 
-    })
-    .keypress(function (e) {
+   $(".pure-input-1-2") // <- Add this
+	.keypress(function (e) {
         var key = e.which;
         console.log(key);
-        if(key == 13) 
-            {
-                e.preventDefault();
-                $('li').show();
-                $('ul').append('<li><div class="popup"></div><i class="fa fa-check-circle-o fa-2x"><i class="fa fa-times fa-1x"></i></i></li>');
-  }
+        if(key == 13) {
+        	e.preventDefault();
+            $('li').show();
+			var text = $(this).val(); // <- Add these  -v    
+            $('ul').append('<li><div class="popup">' + text + '</div><i class="fa fa-check-circle-o fa-2x"><i class="fa fa-times fa-1x"></i></i></li>');
+		} 
+        $('.fa-times').click(function() {
+        $(this).parents('li').remove();
+    });
     
-
+    $('.fa-check-circle-o').click(function() {
+        $(this).closest('li').css("opacity", "0.4");
+    });
+})
     
+   
     
 });
 
