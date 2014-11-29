788ccc3cbcb17b94891179e7f0bcf4ac84929f70
krj5063 <krj5063@gmail.com>
2014-11-29 12:52:50 -0500

    made changes
    made changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-$(document).ready(function() { }
+$(document).ready(function() { 
+
+    $(".pure-input-1-2")
+    .keyup(function() {
+    var value = $( this ).val();
+    $(".popup").text( value );
+    })
+    .keyup(); 
+    })
+    .keypress(function (e) {
+        var key = e.which;
+        console.log(key);
+        if(key == 13) 
+            {
+                e.preventDefault();
+                $('li').show();
+                $('ul').append('<li><div class="popup"></div><i class="fa fa-check-circle-o fa-2x"><i class="fa fa-times fa-1x"></i></i></li>');
+  }
+    
+
+    
+    
+});
+
